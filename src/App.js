eb5a9693af3ca2240e60cea9ede15ce6ab652480
PreethiAcharya <77332115+PreethiAcharya@users.noreply.github.com>
2025-03-05 21:33:53 +0530

    Refactor all components

--- src/App.js
+++ src/App.js
@@ -1,100 +1,28 @@
-import { Container, Segment, Statistic, Grid, Header, Icon, Form, Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
+import MainHeader from './components/MainHeader';
+import NewEntryForm from './components/NewEntryForm';
+import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
+
 
 function App() {
   return (
     <Container>
-      <Header as="h1">Budget</Header>
+      <MainHeader title="budget"/>
+      <DisplayBalance title="Your Balance" value="1234.54"/>
 
-      <Statistic size="small">
-        <Statistic.Label>Your Balance:</Statistic.Label>
-        <Statistic.Value>25000.53</Statistic.Value>
-      </Statistic>
+      <DisplayBalances/>
 
-      <Segment textAlign='center'>
-        <Grid columns={2} divided>
-          <Grid.Row>
-            <Grid.Column><Statistic size="tiny" color="green">
-              <Statistic.Label style={{ textAlign: "left" }}>
-                Income:
-              </Statistic.Label>
-              <Statistic.Value>1,045.50</Statistic.Value>
-            </Statistic></Grid.Column>
-            <Grid.Column>
-              <Statistic size="tiny" color="red">
-                <Statistic.Label style={{ textAlign: "left" }}>
-                  Expenses:
-                </Statistic.Label>
-                <Statistic.Value>623.50</Statistic.Value>
-              </Statistic>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <MainHeader title="History" type="h3"></MainHeader>
 
-      <Header as='h3'>History</Header>
+      <EntryLine title="something" value="10000"></EntryLine>
+      <EntryLine title="something1" value="20000" isExpense></EntryLine>
 
-      <Segment color="red">
-        <Grid column={3} textAlign="right">
-          <Grid.Row>
-              <Grid.Column width={10} textAlign="left">Something</Grid.Column>
-              <Grid.Column width={3} textAlign="right">$10,00</Grid.Column>
-              <Grid.Column width={3}>
-                <Icon name="edit" bordered/>
-                <Icon name="trash" bordered/>
-              </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
 
-      <Segment color="green">
-        <Grid column={3} textAlign="right">
-          <Grid.Row>
-              <Grid.Column width={10} textAlign="left">Something</Grid.Column>
-              <Grid.Column width={3} textAlign="right">$10,00</Grid.Column>
-              <Grid.Column width={3}>
-                <Icon name="edit" bordered/>
-                <Icon name="trash" bordered/>
-              </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Segment color="red">
-        <Grid column={3} textAlign="right">
-          <Grid.Row>
-              <Grid.Column width={10} textAlign="left">Something</Grid.Column>
-              <Grid.Column width={3} textAlign="right">$10,00</Grid.Column>
-              <Grid.Column width={3}>
-                <Icon name="edit" bordered/>
-                <Icon name="trash" bordered/>
-              </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Header as="h3">Add new Transaction</Header>
-      <Form unstackable>
-        <Form.Group>
-          <Form.Input 
-          icon="tags"
-          width={12}
-          label="Description"
-          placeholder="New shinny thing"></Form.Input>
-        
-        <Form.Input
-        width={4}
-        label="value"
-        placeholder="100.00"
-        icon="dollar"
-        iconPosition='left'></Form.Input>
-        </Form.Group>
-        <Button.Group style={{marginTop: 20}}>
-          <Button>Cancel</Button>
-          <Button.Or/>
-          <Button primary>OK</Button>
-        </Button.Group>
-      </Form>
+      <MainHeader title="Add New Transaction" type="h3"></MainHeader>
+      <NewEntryForm/>
     </Container>
   );
 }
